f2d6d0c8ef1c30d0a46002b63037f8e31f341ec7
Nikilesh <nxsambamurthy@shockers.wichita.edu>
2022-11-20 23:07:25 -0600

    Added viewmodel.

---
+++ ViewModels/HistoryViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace Calculator.ViewModels
+{
+    public partial class HistoryViewModel:ObservableObject
+    {
+        public HistoryViewModel()
+        {
+            Expr = new ObservableCollection<string>();
+        }
+
+        [ObservableProperty]
+        ObservableCollection<string> expr;
+
+        public static string exprString = "";
+
+        [RelayCommand]
+        public void AddExpr()
+        {
+            if (string.IsNullOrEmpty(exprString))
+            {
+                return;
+            }
+            Expr.Add($" ⟹ {exprString}");
+        }
+    }
+}
