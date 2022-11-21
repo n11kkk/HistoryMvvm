d53f6a8f9eb71a207d6c63321d50b7bd8b6226a9
kartheek <bxmeda@shockers.wichita.edu>
2022-11-20 23:31:27 -0600

    Added the history page.

---
+++ HistoryPage.xaml.cs
@@ -0,0 +1,11 @@
+using Calculator.ViewModels;
+
+namespace Calculator;
+
+public partial class HistoryPage : ContentPage
+{
+    public HistoryPage()
+    {
+        InitializeComponent();
+    }
+}
